1f0220950bcbcf1f0c40080c110b54c20dcf50a7
jorgecc <jcastro@eftec.cl>
2017-05-09 18:54:04 -0300

    1.8 Example of json

---
+++ examples/TestJsonController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright (c) 2016-2017 Jorge Patricio Castro Castillo MIT License.
+ */
+include "../BladeOne.php";
+use eftec\bladeone;
+
+$views = __DIR__ . '/views';
+$compiledFolder = __DIR__ . '/compiled';
+$blade=new bladeone\BladeOne($views,$compiledFolder);
+define("BLADEONE_MODE",1); // (optional) 1=forced (test),2=run fast (production), 0=automatic, default value.
+
+$drinks= array('Cocacola','Pepsi','Fanta','Sprite','7up');
+$json=json_encode($drinks);
+
+header('Content-Type: application/json');
+echo $blade->run("TestJson.example",['json'=>$json]);
+
