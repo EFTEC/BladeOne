b700e34b846ff1d2f5cc866664917be7bd2310f9
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-09-17 12:56:55 +0200

    Add tests for extends and basic variable printin

---
+++ tests/VariablesTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace eftec\tests;
+
+/**
+ * Test passing variables into Blade templates.
+ *
+ * @author G.J.W. Oolbekkink <g.j.w.oolbekkink@gmail.com>
+ * @since 17/09/2018
+ */
+class VariablesTest extends AbstractBladeTestCase {
+    /**
+     * @throws \Exception
+     */
+    public function testPrintVariable() {
+        $bladeString = /** @lang Blade */
+            <<<'BLADE'
+{{$var1}}
+BLADE;
+        $this->assertEqualsIgnoringWhitespace("content", $this->blade->runString($bladeString, ["var1" => "content"]));
+        $this->assertEqualsIgnoringWhitespace("content2", $this->blade->runString($bladeString, ["var1" => "content2"]));
+        $this->assertEqualsIgnoringWhitespace("&lt;a href=&quot;/&quot;&gt;My Link&lt/a&gt;", $this->blade->runString($bladeString, ["var1" => "<a href=\"/\">My Link</a>"]));
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testPrintUnescapedVariable() {
+        $bladeString = /** @lang Blade */
+            <<<'BLADE'
+{!! $var1 !!}
+BLADE;
+        $this->assertEqualsIgnoringWhitespace("content", $this->blade->runString($bladeString, ["var1" => "content"]));
+        $this->assertEqualsIgnoringWhitespace("<a href=\"/\">My Link</a>", $this->blade->runString($bladeString, ["var1" => "<a href=\"/\">My Link</a>"]));
+    }
+}
---
+++ tests/resources/templates/extends_child_section.blade.php
@@ -0,0 +1,5 @@
+@extends('extends_base')
+
+@section('from_child')
+    From Child...
+@endsection
