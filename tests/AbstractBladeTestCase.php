ade44924c957abe9dafb9ec0c4d3d97030cf1a69
Jorge Castro <jorge.castro.c@gmail.com>
2019-03-01 18:18:07 -0300

    3.20

--- tests/AbstractBladeTestCase.php
+++ tests/AbstractBladeTestCase.php
@@ -18,11 +18,14 @@ abstract class AbstractBladeTestCase extends TestCase {
         parent::__construct($name, $data, $dataName);
         $this->blade = new BladeOne(self::TEMPLATE_PATH, self::COMPILED_PATH, BladeOne::MODE_SLOW);
     }
-
-    protected function tearDown() {
+	/*
+    // tearDown on php7.2 is implemented as tearDown():void. However PHP 5.6 doesn't allows it.
+	// So I comment this line because it breaks Travis.
+	protected function tearDown() {
         // Remove files compiled in this test to prevent side effects.
         array_map('unlink', glob(self::COMPILED_PATH . "/*"));
     }
+	*/
 
     public function assertEqualsIgnoringWhitespace($expected, $actual, $message = '', $delta = 0.0, $maxDepth = 10, $canonicalize = false, $ignoreCase = false) {
         $this->assertEquals(
