dee1bb4678d4540e95e6b9bd963035a7c2f0e95b
Jorge Castro <jorge.castro.c@gmail.com>
2019-03-07 18:51:26 -0300

    3.21 beta

--- examples/testinclude.php
+++ examples/testinclude.php
@@ -3,17 +3,14 @@
  * Copyright (c) 2019 Jorge Patricio Castro Castillo MIT License.
  */
 
-
 use eftec\bladeone\BladeOne;
 
 include "../lib/BladeOne.php";
-
 $blade=new BladeOne(null,null, BladeOne::MODE_DEBUG);
 /** examples/views/Shared/input.blade.php */
 $blade->addInclude('Shared.input', 'input');
 /** examples/views/Shared/input2.blade.php */
 $blade->addInclude('Shared.input2'); 
-
-echo $blade->run("Test.include"
-    , ["title" => "VARIABLE TITLE GOES HERE"]);
+/** examples/views/Test/include.blade.php */
+echo $blade->run("Test.include", ["title" => "VARIABLE TITLE GOES HERE"]);
 
