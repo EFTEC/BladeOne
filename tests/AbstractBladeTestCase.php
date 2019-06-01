100ee940099c5ff45ab44536374feb70b7c5b780
Jake Whiteley <odegramusic@gmail.com>
2019-06-01 23:08:51 +0100

    Add @component tests

--- tests/AbstractBladeTestCase.php
+++ tests/AbstractBladeTestCase.php
@@ -7,27 +7,39 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * @author G.J.W. Oolbekkink <g.j.w.oolbekkink@gmail.com>
- * @since 16/09/2018
+ * @since  16/09/2018
  */
-abstract class AbstractBladeTestCase extends TestCase {
+abstract class AbstractBladeTestCase extends TestCase
+{
     const TEMPLATE_PATH = __DIR__ . '/resources/templates';
     const COMPILED_PATH = __DIR__ . '/resources/compiled';
 
     protected $blade;
-    public function __construct($name = null, array $data = [], $dataName = '') {
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
         parent::__construct($name, $data, $dataName);
         $this->blade = new BladeOne(self::TEMPLATE_PATH, self::COMPILED_PATH, BladeOne::MODE_SLOW);
     }
-	/*
+
+    /*
     // tearDown on php7.2 is implemented as tearDown():void. However PHP 5.6 doesn't allows it.
-	// So I comment this line because it breaks Travis.
-	protected function tearDown() {
+    // So I comment this line because it breaks Travis.
+    protected function tearDown() {
         // Remove files compiled in this test to prevent side effects.
         array_map('unlink', glob(self::COMPILED_PATH . "/*"));
     }
-	*/
+    */
 
-    public function assertEqualsIgnoringWhitespace($expected, $actual, $message = '', $delta = 0.0, $maxDepth = 10, $canonicalize = false, $ignoreCase = false) {
+    public function assertEqualsIgnoringWhitespace(
+        $expected,
+        $actual,
+        $message = '',
+        $delta = 0.0,
+        $maxDepth = 10,
+        $canonicalize = false,
+        $ignoreCase = false
+    ) {
         $this->assertEquals(
             preg_replace('/\s/', '', $expected),
             preg_replace('/\s/', '', $actual),
