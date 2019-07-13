2588829b8c90d9bb25999c1ad51335918f7e9a10
Jorge Castro <jorge.castro.c@gmail.com>
2019-07-13 10:48:14 -0400

    examples

--- examples/views/Test/hello2.blade.php
+++ examples/views/Test/hello2.blade.php
@@ -1,26 +1,14 @@
 @extends('Shared.newlayout')
 
-@section('title', 'Page Title')
-
-@section('sidebar')
-    @include('Test.sometemplate')
-    <p style="background-color: darkgrey">It is a sidebar created in hello2.blade.php. It could be used as many times as you want
-    @parent
-    </p>
-
-    <hr>assets:
-    @asset('aaa.js')
-
-@endsection
-
-@section('sidebar2')
-    <p style="background-color: darkgrey">It is a sidebar2 created in hello2.blade.php. It could be used as many times as you want
-        @parent
-    </p>
-@endsection
 
 @section('footer')
     <p style="background-color: darkgrey">It is the footer. This footer is yield so it shouldn't contain a parent
         @parent this parent is completely ignored.
     </p>
+@endsection
+
+@section('footermissing')
+    <p style="background-color: darkgrey">If you are reading this, then footermissing is not missing.
+    </p>
+    <p>@@parent Don't add this one.</p> 
 @endsection
