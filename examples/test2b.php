d816fb8cd35ee709b05c61c54f3188e1f2c1b899
Jorge Castro <jorge.castro.c@gmail.com>
2019-07-13 10:21:24 -0400

    examples

---
+++ examples/test2b.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (c) 2016 Jorge Patricio Castro Castillo MIT License.
+ */
+include "../lib/BladeOne.php";
+use eftec\bladeone\BladeOne;
+
+$views = __DIR__ . '/views';
+$compiledFolder = __DIR__ . '/compiled';
+$blade=new BladeOne($views,$compiledFolder,BladeOne::MODE_DEBUG);
+
+
+
+//<editor-fold desc="Example data">
+$v1=1;
+
+try {
+    echo $blade->run("Test.hello2", ['other' => 'hello world', 'v1' => $v1]);
+} catch (Exception $e) {
+    echo "error found ".$e->getMessage()."<br>".$e->getTraceAsString();
+}
